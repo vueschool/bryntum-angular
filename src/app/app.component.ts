3afdd36107eed61ae88bd73af964f7d9d7f7c0a6
Daniel Kelly <me@danielkelly.io>
2023-11-02 17:10:40 -0500

    welcome video code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BryntumSchedulerComponent } from '@bryntum/scheduler-angular';
+import { SchedulerConfig } from '@bryntum/scheduler';
+
+const schedulerConfig: Partial<SchedulerConfig> = {
+  viewPreset: 'hourAndDay',
+  startDate: new Date(2024, 2, 20, 6),
+  endDate: new Date(2024, 2, 20, 20),
+  crudManager: {
+    loadUrl: '/assets/data.json',
+    autoLoad: true,
+  },
+  columns: [
+    { text: 'Name', field: 'name', width: 130 },
+    { text: 'Age', field: 'age', width: 75 },
+  ],
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bryntum-angular';
+  schedulerConfig = schedulerConfig;
+  @ViewChild('scheduler') schedulerComponent!: BryntumSchedulerComponent;
 }
